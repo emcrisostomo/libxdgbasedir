be4c4e70cfac321be7dd29faf26aebf634a65745
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2017-08-29 02:24:44 +0200

    Remove placeholder files

--- src/libxdgbasedir/library.cpp
+++
@@ -1,9 +0,0 @@
-#include "library.h"
-
-#include <iostream>
-#include "gettext_defs.h"
-
-void hello()
-{
-  std::cout << _("Hello, World!") << std::endl;
-}
--- src/libxdgbasedir/library.h
+++
@@ -1,4 +0,0 @@
-#ifndef LIBXDGBASEDIR_LIBRARY_H
-#define LIBXDGBASEDIR_LIBRARY_H
-void hello();
-#endif
